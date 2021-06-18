98102ff3a58ee0d01741656c7fdb786d4239dcb2
Kyle Givler <xvegan88x@gmail.com>
2021-06-18 18:02:16 -0400

    Convert warn command

--- Cicada 122723/Commands/Moderation.cs
+++ Cicada 122723/Commands/Moderation.cs
@@ -13,6 +13,96 @@ namespace Jupiter.Commands
 
         }
 
+        [RequireOwner]
+        [Command("warn")]
+        public async Task Warn(SocketUser user, [Remainder]string warning = null)
+        {
+            EmbedBuilder emb = new EmbedBuilder();
+
+            if (Context.Message.Author.Username == "TheRedLord" || Context.Message.Author.Username ==  "JoyfulReaper")
+            {
+                Console.WriteLine("the warning: " + warning);
+
+
+                //msg.Channel.DeleteMessageAsync(msg);
+                if (Context.Message.Content.Contains("for"))
+                {
+                    /*
+                    foreach (char c in command)
+                    {
+                        if (c != ' ')
+                        {
+                            warning += c;
+
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        if (start == true)
+                        {
+                            warning += c;
+                        }
+                        if (get_reason == "for")
+                        {
+                            reason += c;
+                        }
+                        if (c == '<')
+                        {
+                            start = true;
+                            warning += c;
+
+                        }
+                        else if (c == '>')
+                        {
+                            start = false;
+                        }
+                        if (c == 'f')
+                        {
+                            get_reason += c;
+                        }
+                        else if (get_reason == "f")
+                        {
+                            get_reason += c;
+                        }
+                        else if (get_reason == "fo")
+                        {
+                            get_reason += c;
+                        }
+                    }
+
+                    Console.WriteLine("reason: " + reasonForWarning);
+                    emb = new EmbedBuilder();
+                    emb.WithColor(Discord.Color.DarkBlue);
+                    emb.AddField("This is a warning", warning + " you have been warned for" + reasonForWarning);
+                    await msg.Channel.SendMessageAsync("", false, emb.Build());
+                    Console.WriteLine(warning);
+                    warning = "";
+                    reasonForWarning = "";
+                    get_reason = "";
+                    */
+                }
+                else
+                {
+                    if (warning == null)
+                    {
+                        await ReplyAsync(Context.Message.Author.Mention + " please specify the reason");
+                        return;
+                    }
+
+                    emb = new EmbedBuilder();
+                    emb.AddField("User", $"{user.Mention}You have been warned by " + Context.Message.Author.Mention);
+                    emb.AddField("Warning", $"You have been warned for: {warning}");
+                    emb.WithColor(Color.Red);
+                    await ReplyAsync("", false, emb.Build());
+                }
+            }
+            else
+            {
+                await ReplyAsync(@"⛔" + Context.Message.Author.Mention + " you dont have the right role" + @" ⛔");
+            }
+        }
+
         [Command("edit")]
         public async Task Edit(string edit = null)
         {
